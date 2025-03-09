638daa4d4f9ffb8a1a1dd4c2fd252cff1b4e73d4
florida <tonakpaflorida99@gmail.com>
2025-03-09 01:15:15 +0100

    ah

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const touches = [...document.querySelectorAll('.bouton')]; 
 const listeKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('ecran')
 
 document.addEventListener('keydown', (e) => {
     const valeur = e.keyCode.tiString() 
@@ -13,8 +14,20 @@ document.addEventListener('click', (e) => {
 const calculer = (valeur) => {
     if(listeKeycode.includes(valeur)) {
         switch(valeur) {
-            
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.index0f(valeur);
+                const touche = touches [indexKeycode]
+                ecran.textContent += touche.innerHTML;
         }
         
     }
 }
+
+window.addEventListener('error', (e) => console.log(e))
